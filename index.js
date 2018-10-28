a367115b390881d03f646fc1a8b3f7fa49d1eab1
Joaquín Martínez Peréz <jmpjoaquinjmp@gmail.com>
2018-10-28 02:07:53 +0100

    Update index.js
    json en minusculas.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded( { extended : false } ))
-app.use( bodyParser.Json() )
+app.use( bodyParser.json() )
 
 app.get( '/' , (req , res) => {
 	res.send( " Al fin jornadas." )
