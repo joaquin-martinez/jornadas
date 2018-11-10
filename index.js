87c2c1953940914c1834c9d1f5446feba03f5084
joaquin-martinez <joamovil@yahoo.es>
2018-11-10 19:44:33 +0100

    quito req ejs y cambio include

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path =require('path')
-const ejs = require('ejs')
+// const ejs = require('ejs')
 // const html = require('http')
 
 
