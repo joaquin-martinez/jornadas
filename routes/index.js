deb398cf7afd910b761ba5b5bbf1c0ddd8997edf
joaquin-martinez <joamovil@yahoo.es>
2018-11-22 12:00:47 +0100

    cambio posicion config

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const express = require('express')
 const api = express.Router()
 const userCtr = require('../controllers/userControllers')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
 const Config = require('./config')
+const MongoStore = require('connect-mongo')(session)
+
 
 api.use(session({
   secret : "afr33ty5",
