c22c3ae73393abbb66ff1edeeb57bfa65df942ff
joaquin-martinez <joamovil@yahoo.es>
2018-11-22 11:09:25 +0100

    require express-session en rutas.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const api = express.Router()
 const userCtr = require('../controllers/userControllers')
-
+const session = require('express-session')
 
 api.get( '/' , (req , res) => {
 
