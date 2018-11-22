5259e4d40af2d4d543eb9482b7fe19352d5d910b
joaquin-martinez <joamovil@yahoo.es>
2018-11-22 10:45:48 +0100

    Problemas al crear variables de sesion.

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -31,7 +31,8 @@ function getUser ( req , res ) {
 	if(err)console.log('error en la busqueda');
 	else if (trabajador[0] && candidato.usuario == trabajador[0].user ){
 		console.log( list);
-    req.session.user = trabajador[0].tipo
+    console.log(trabajador[0].tipo);
+    req.session.user = trabajador[0].tipo;
 		res.render('entrada' , {tipo : req.session.user.tipo})
 								}
 	else{
