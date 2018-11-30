339b85a13c40706da695610358f4ce4374feebc0
joaquin-martinez <joamovil@yahoo.es>
2018-11-30 13:05:44 +0100

    controles puestos

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -96,8 +96,11 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function(e) {
+  console.log("entra");
   e.preventDefault();
+  console.log("sigue");
     modal.style.display = "block";
+    console.log("termina");
 }
 
 // When the user clicks on <span> (x), close the modal
