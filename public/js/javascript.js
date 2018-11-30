e5e4023116d1a4c53d61719e851e75cdf0771bef
joaquin-martinez <joamovil@yahoo.es>
2018-11-30 16:45:52 +0100

    modal emp

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -86,32 +86,37 @@ $.get('./jornadas', (data)=>{
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modalUsu = document.getElementById("usuModal");
+var modalEmp = document.getElementById("empModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("altaUsu");
+var btnUsu = document.getElementById("altaUsu");
+var btnEmp = document.getElementById("altaEmp");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
-btn.addEventListener("click" ,function() {
+btnUsu.addEventListener("click" ,function() {
   console.log("entra");
 //  e.preventDefault();
 //  console.log("sigue");
-    modal.style.display = "block";
+    modalUsu.style.display = "block";
     console.log("termina");
 });
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modalUsu.style.display = "none";
+    modalEmp.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modalUsu.style.display = "none";
+        modalEmp.style.display = "none";
     }
 }
 
