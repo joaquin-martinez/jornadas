514adb401942ed46b2c7a30b8efb9c050228b16a
joaquin-martinez <joamovil@yahoo.es>
2018-12-02 12:45:53 +0100

    mal puesto $.post

--- public/js/logjavascript.js
+++ public/js/logjavascript.js
@@ -4,7 +4,7 @@ $("#datosValidacion").on( "submit" , (e)=>{
   e.preventDefault();
   let datos = e.target.result;
   console.log(datos);
-  $post("./app" , datos , function(err , data){
+  $.post("./app" , datos , function(err , data){
     if(err){
       $("#salidaValidacion").html("Error en el proceso de validación.");
 
