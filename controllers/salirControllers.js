f67476fd34353aee07ddb70c5a522e5841c3e920
joaquin-martinez <joamovil@yahoo.es>
2018-12-02 21:42:48 +0100

    ++++

--- controllers/salirControllers.js
+++ controllers/salirControllers.js
@@ -4,7 +4,7 @@ const salir = ( req , res )=>{
   req.session.destroy()
   console.log("Estamos fuera");
   res.status(200)
-  res.send("");
+//  res.send("");
   res.redirect("/home/node/jornadas/public/index.html")
 }
 
