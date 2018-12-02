b61132675e71a97edea29c11a159e161d4454f63
joaquin-martinez <joamovil@yahoo.es>
2018-12-02 12:33:32 +0100

    separo public js

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -138,20 +138,6 @@ $("#salgo").click(()=>{
 });
 
 
-// Validacion.
-$("#datosValidacion").on( "submit" , (e)=>{
-  e.preventDefault();
-  let datos = e.target.result;
-  console.log(datos);
-  $post("./app" , datos , function(err , data){
-    if(err){
-      $("#salidaValidacion").html("Error en el proceso de validación.");
-
-    } else {
-      $("#salidaValidacion").html("Usuario o clave incorrecta.......");
-    }
-  });
-
 });
 
 
---
+++ public/js/logjavasript.js
@@ -0,0 +1,14 @@
+
+// Validacion.
+$("#datosValidacion").on( "submit" , (e)=>{
+  e.preventDefault();
+  let datos = e.target.result;
+  console.log(datos);
+  $post("./app" , datos , function(err , data){
+    if(err){
+      $("#salidaValidacion").html("Error en el proceso de validación.");
+
+    } else {
+      $("#salidaValidacion").html("Usuario o clave incorrecta.......");
+    }
+  });
