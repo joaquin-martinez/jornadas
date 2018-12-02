033921d8d24a8155f3605a28fef07143731a4d76
joaquin-martinez <joamovil@yahoo.es>
2018-12-02 23:35:31 +0100

    middle

--- middlewares/logado.js
+++ middlewares/logado.js
@@ -1,5 +1,7 @@
 module.exports = ( req , res , next )=>{
   if (!req.session.user) {
-    res.status(404).sen({mensaje : "Tiene que logarse"})
+    res.status(404).send({mensaje : "Tiene que logarse"})
+  }else {
+    next();
   }
 }
