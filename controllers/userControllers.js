c899da8354090392ee4e57a59f4e4b4264664134
joaquin-martinez <joamovil@yahoo.es>
2018-12-05 22:57:24 +0100

    prueba con google

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -18,8 +18,8 @@ function setUser( req , res ) {
 
       console.log(user);
 //      res.status(200)
-      res.redirect("../recarga")
-      res.render('entrada' , {tipo : req.session.user.tipo , user : req.session.user.user})
+      res.redirect("http://google.es")
+//      res.render('entrada' , {tipo : req.session.user.tipo , user : req.session.user.user})
     }
 
   })
