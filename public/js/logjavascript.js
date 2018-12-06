954ec0b144f0a242efede08e988670df3c7c2724
joaquin-martinez <joamovil@yahoo.es>
2018-12-06 05:30:17 +0100

    entrada para movil

--- public/js/logjavascript.js
+++ public/js/logjavascript.js
@@ -1,19 +1,24 @@
 $(()=>{
 // Validacion.
-$("#datosValidacion").on( "submit" , (e)=>{
+// $("#datosValidacion").on( "submit" , (e)=>{
+datosValidacion.submit =  (e)=>{
   e.preventDefault();
 //  let datos = new FormData(datosValidacion);
 let datos = {
-  usuario : $("#usuario").val(),
-  clave : $("#clave").val()
+//  usuario : $("#usuario").val(),
+  usuario : usuario.value,
+//  clave : $("#clave").val()
+  clave : clave.value
 
 };
   console.log(datos);
   $.post("./app" , datos , function(data){
     if(data.mensaje){
       datosValidacion.reset();
-      $("#salidaValidacion").html("validacion incorrecta. intentelo de nuevo."
-     + data.mensaje );
+//      $("#salidaValidacion").html("validacion incorrecta. Intentelo de nuevo."
+      salidaValidacion.innerHTML = "validacion incorrecta. Intentelo de nuevo."
+//     + data.mensaje );
+     + data.mensaje ;
 
     } else {
 //      window.open("./app" , "_self" );
