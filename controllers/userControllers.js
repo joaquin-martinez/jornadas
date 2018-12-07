2ee3849ba3e6b3972b4b503ec844ecd075f4cbee
joaquin-martinez <joamovil@yahoo.es>
2018-12-07 17:27:12 +0100

    ponemos $set en put

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -80,8 +80,8 @@ function validateUser ( req , res ) {
 
   function putUser ( req , res ) {
     console.log(`Actualizando usuario ${req.body.musuario}`);
-    Usuar.findOneAndUpdate({user : req.body.usuario} , { password : req.body.mclave ,
-     tipo : req.body.mtipo } , (err, raw)=>{
+    Usuar.findOneAndUpdate({user : req.body.usuario} ,{ $set : { password : req.body.mclave ,
+     tipo : req.body.mtipo }} , (err, raw)=>{
       if (err) return handleError(err);
       console.log('The raw response from Mongo was ', raw);
               res.status(200).redirect("../recarga")
